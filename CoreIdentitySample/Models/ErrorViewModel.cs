d33f3be5cada36b5761b6355a2dc5254146972f8
osman-sahin <osmansahin.eng@gmail.com>
2020-06-10 16:56:11 +0300

    Add project files.

---
+++ CoreIdentitySample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreIdentitySample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
